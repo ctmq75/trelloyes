eaf388a0331515e1b2ac644533e89c6da45686fe
Chris McArdle <christopher.mcardle2@gmail.com>
2020-05-12 12:33:26 -0500

    Updates

---
+++ precache-manifest.6dc11cc4b65d0e39c5b3901c8de1391b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cedc8e32d0fcaa7d9bec520c0d31b55",
+    "url": "/trelloyes/index.html"
+  },
+  {
+    "revision": "00b9611dc680e3f400c2",
+    "url": "/trelloyes/static/css/main.a8452b03.chunk.css"
+  },
+  {
+    "revision": "2ee6322da49ab63640f0",
+    "url": "/trelloyes/static/js/2.79f8b80c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trelloyes/static/js/2.79f8b80c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00b9611dc680e3f400c2",
+    "url": "/trelloyes/static/js/main.4f93dda1.chunk.js"
+  },
+  {
+    "revision": "473f8c522978ab01e751",
+    "url": "/trelloyes/static/js/runtime-main.7f04bbda.js"
+  }
+]);
