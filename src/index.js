facdf16663d8df0d7ca13068fbdc25fa19e7252c
Chris McArdle <christopher.mcardle2@gmail.com>
2020-05-15 10:53:55 -0500

    added functionality

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
   
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import STORE from './store'
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
 
 
 
 ReactDOM.render(
-  <App store={STORE} />,
+  <App  />,
   document.getElementById('root'),
 );
